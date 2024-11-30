298e8bc92a7589252a5622c1565c9a2ca6d93783
victordalet <victordalet@protonmail.com>
2024-12-01 00:30:39 +0100

    feat(main): Add default setting to run

--- README.md
+++ README.md
@@ -1,1 +1,42 @@
 # ai_to_old_car
+
+
+---
+
+
+## 1. Compilation
+
+---
+
+```
+mkdir build-dir
+cd build-dir
+cmake ..
+make
+```
+
+
+## 2. Run
+
+---
+
+```
+./ai_to_old_car <path_to_onnx_model>
+```
+
+
+## 3. Environment
+
+
+---
+
+- `DEBUG`: Display screen output
+
+
+## 4. Linter
+
+---
+
+```
+cpplint 
+```
--- main.cpp
+++ main.cpp
@@ -1,10 +1,8 @@
 #include <iostream>
 #include <opencv2/opencv.hpp>
-#include "src/yolo.h"
 
 
 int main(int argc, char const *argv[]) {
-
 
 
     if (argc != 2) {
@@ -12,10 +10,7 @@ int main(int argc, char const *argv[]) {
       return 1;
     }
 
-
     cv::VideoCapture cap(0);
-
-    InferenceEngine engine(argv[1]);
 
 
     while (true) {
@@ -24,32 +19,22 @@ int main(int argc, char const *argv[]) {
         cap >> frame;
 
 
-        int orig_width = frame.cols;
-        int orig_height = frame.rows;
-        auto timer = cv::getTickCount();
-
-        std::vector<float> input_tensor_values = engine.preprocessImage(frame);
-
-        std::vector<float> results = engine.runInference(input_tensor_values);
-
-        float confidence_threshold = 0.3;
-
-        std::vector<Detection> detections = engine.filterDetections(results, confidence_threshold, engine.input_shape[2], engine.input_shape[3], orig_width, orig_height);
-
-        double fps = cv::getTickFrequency() / ((double)cv::getTickCount() - timer);
-
-        cv::putText(frame, "FPS: " + std::to_string(fps), cv::Point(10, 30), cv::FONT_HERSHEY_SIMPLEX, 1, cv::Scalar(0, 255, 0), 2, 8);
-
-        cv::Mat output = engine.draw_labels(frame, detections);
+        if (std::getenv("DEBUG") != NULL) {
+            cv::imshow("Video", frame);
+        }
 
 
-        cv::imshow("Video", frame);
-        if (cv::waitKey(30) & 0xFF == 27) {
+        if (cv::waitKey(1) == 'q') {
             break;
         }
 
 
     }
+
+
+    cap.release();
+
+    cv::destroyAllWindows();
 
     return 0;
 }
