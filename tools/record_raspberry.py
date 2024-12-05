971e3d164810a2b3e052f5df3f9c97e0c5dae9bf
victordalet <victordalet@protonmail.com>
2024-12-05 22:49:20 +0100

    fix(tools): Update raspberry script

--- tools/record_raspberry.py
+++ tools/record_raspberry.py
@@ -1,15 +1,8 @@
-import picamera
 import time
+import picamera2
 
-
-def main():
-    camera = picamera.PiCamera()
-    camera.resolution = (640, 480)
-    camera.start_recording(f"output_{time.time()}.h264")
-    camera.wait_recording(60)
-    camera.stop_recording()
-
-
-if __name__ == "__main__":
-    while True:
-        main()
+camera = picamera2.Picamera2()
+camera.resolution = (640, 480)
+while True:
+    print(time.time())
+    camera.start_and_record_video(f"output_{time.time()}.mp4", duration=20)
